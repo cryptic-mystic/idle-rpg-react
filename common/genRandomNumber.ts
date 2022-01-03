222d45cf836a11168a5c4fc6afe37bd1cf601d1b
John Crowe <john.byron.crowe@gmail.com>
2022-01-03 15:57:06 -0600

    feat: adding discovery of an initial home

---
+++ common/genRandomNumber.ts
@@ -0,0 +1,3 @@
+const genRandomNumber = (max: number = 1, min: number = 0) => Math.floor(Math.random() * (max - min) + min)
+
+export default genRandomNumber
--- context/PlayerContext.tsx
+++ context/PlayerContext.tsx
@@ -3,29 +3,38 @@ import React, {
   useCallback,
   useState,
   useContext,
+  useEffect,
 } from 'react'
+
+import genRandomNumber from '~/common/genRandomNumber'
 
 import AdventureLogContext from './AdventureLogContext'
 
 interface PlayerContext {
   metersWalked: number
   travelling: boolean
+  foundHome: boolean
   walkForOneMinute?: () => void
 }
 
 const PlayerContext = React.createContext<PlayerContext>({
   metersWalked: 0,
   travelling: false,
+  foundHome: false
 })
+
+const FIND_HOME_DISTANCE_METERS = genRandomNumber(120, 60)
 
 export const PlayerProvider = ({ children }: PropsWithChildren<{}>) => {
   const { addAdventureLog } = useContext(AdventureLogContext)
 
   const [metersWalked, setMetersWalked] = useState(0)
-  const [walking, setWalking] = useState(false)
+  const [travelling, setTravelling] = useState(false)
+
+  const [foundHome, setFoundHome] = useState(false)
 
   const walkForOneMinute = useCallback(() => {
-    if (walking) return
+    if (travelling) return
 
     if (metersWalked === 0)
       addAdventureLog('You begin exploring the surrounding forest...')
@@ -34,17 +43,25 @@ export const PlayerProvider = ({ children }: PropsWithChildren<{}>) => {
       setMetersWalked((currentMetersWalked) => currentMetersWalked + 1.4)
     }, 1000)
 
-    setWalking(true)
+    setTravelling(true)
 
     setTimeout(() => {
       clearInterval(walkingInterval)
-      setWalking(false)
-    }, 60000)
-  }, [metersWalked, walking])
+      setTravelling(false)
+    }, 60000 * 2)
+  }, [metersWalked, travelling])
+
+  // Looking for a players first home
+  useEffect(() => {
+    if (metersWalked >= FIND_HOME_DISTANCE_METERS && !foundHome) {
+      setFoundHome(true)
+      addAdventureLog("You found a cave that should serve as a decent shelter for a while...")
+    }
+  }, [metersWalked, foundHome])
 
   return (
     <PlayerContext.Provider
-      value={{ metersWalked, travelling: walking, walkForOneMinute }}
+      value={{ metersWalked, travelling, foundHome, walkForOneMinute }}
     >
       {children}
     </PlayerContext.Provider>
